8863b2c15d745ffd73fe138c5b420c2d1d65e115
kejriwaldivya <36460544+kejriwaldivya@users.noreply.github.com>
2018-03-05 14:25:52 -0800

    First Commit
    Initianl

---
+++ Assets/Scripts/CameraStable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour {
+
+    public GameObject car;
+    public float Xpos;
+    public float Ypos;
+    public float Zpos;
+
+	void Update () {
+        Xpos = car.transform.eulerAngles.x;
+        Ypos = car.transform.eulerAngles.y;
+        Zpos = car.transform.eulerAngles.z;
+        transform.eulerAngles = new Vector3(Xpos - Xpos, Ypos, Zpos - Zpos);
+    }
+}
